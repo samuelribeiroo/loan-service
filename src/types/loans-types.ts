42ffc539daddce931b54b870301c39fcf0f34751
samuelribeiroo <samuelribeirodev@gmail.com>
2024-08-14 22:40:37 -0300

    update: loan services type

--- src/types/loans-types.ts
+++ src/types/loans-types.ts
@@ -1,4 +1,4 @@
-export type LoanServcice = "PERSONAL" | "GUARANTEED" | "ASSIGNED"
+export type LoanServcice = "PERSONAL" | "GUARANTEED" | "CONSIGNMENT"
 
 export type LoanReport = {
   customer: string
