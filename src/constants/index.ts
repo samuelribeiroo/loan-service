0f9cd6ed4563cab6db4d725496566784b72dc24c
samuelribeiroo <samuelribeirodev@gmail.com>
2024-08-16 15:46:28 -0300

    refactor: run lint at file

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,13 @@
 enum LoanTypes {
   Personal = "PERSONAL_LOAN",
   Guaranteed = "GUARANTEED_LOAN",
-  CONSIGNMENT = "CONSIGNMENT_LOAN",
+  Consigment = "CONSIGNMENT_LOAN",
 }
 
 export const LoansInterestRates = {
   [LoanTypes.Personal]: 0.04,
   [LoanTypes.Guaranteed]: 0.03,
-  [LoanTypes.CONSIGNMENT]: 0.02,
+  [LoanTypes.Consigment]: 0.02,
 }
 
 // The interest rates are being using in a decimal format to maintain the consistency and turn easy the calculus.
