e72750ad8d186a9fa329c3fa4712351e25ecbd04
samuelribeiroo <samuelribeirodev@gmail.com>
2024-08-18 20:12:55 -0300

    update: sql schema type accepted

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE clients_register(
     id INTEGER PRIMARY KEY NOT NULL,
     name VARCHAR(150) NOT NULL,
     age INTEGER NOT NULL CHECK (age >= 18 AND age <= 99),
-    cpf VARCHAR(11) NOT NULL,
+    cpf VARCHAR(14) NOT NULL,
     income DECIMAL(10, 2) NOT NULL,
     location VARCHAR(150) NOT NULL
 );
