933d4d1dd9b83cbd974b8f40121eb49497ebcd04
samuelribeiroo <samuelribeirodev@gmail.com>
2024-08-19 09:33:59 -0300

    feat: avoid duplicated customers based on cpf

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -7,13 +7,24 @@ class Customer {
   async create(request: Request, response: Response) {
     try {
       const customerData = request.body
+
       if (request.body === "") {
         return response.sendStatus(400)
       }
+
       const newCustomer = await createCustomer(customerData)
+
       return response.sendStatus(201).json({ customer: newCustomer })
     } catch (error) {
-      console.log(error)
+      if (error instanceof Error) {
+        if (error.message === "CPF INSERIDO JÁ EXISTE") {
+          return response.sendStatus(409).json({ error: 'CPF inserido já cadastrado.' })
+        } else {
+          return response.json({ message: error.message })
+        }
+      } else {
+        return response.status(500).json({ message: "Erro desconhecido." })
+      }
     }
   }
 
@@ -23,7 +34,7 @@ class Customer {
 
       return response.json({ costumersList }).sendStatus(200)
     } catch (error) {
-      return response.json({error: `Houve algum erro: ${error}`})
+      return response.json({ error: `Houve algum erro: ${error}` })
     }
   }
 }
--- src/services/customerService.ts
+++ src/services/customerService.ts
@@ -8,15 +8,30 @@ export const createCustomer = async (data: {
   income: number
   location: string
 }) => {
-  const createNewCustomer = `INSERT INTO client_register (id, name, age, cpf, income, location)
+  try {
+    const cpfExistQuery = `SELECT COUNT(*) FROM client_register WHERE cpf = $1`
+    const result = await Query(cpfExistQuery, [data.cpf])
+
+    if (result[0].count > 0) {
+      throw new Error("CPF INSERIDO JÁ EXISTE")
+    }
+
+    const createNewCustomer = `INSERT INTO client_register (id, name, age, cpf, income, location)
   VALUES($1, $2, $3, $4, $5, $6)
   RETURNING *
   `
-  const expectedValues = [data.id, data.name, data.age, data.cpf, data.income, data.location]
+    const expectedValues = [data.id, data.name, data.age, data.cpf, data.income, data.location]
 
-  const getResult = await Query(createNewCustomer, expectedValues)
+    const getResult = await Query(createNewCustomer, expectedValues)
 
-  return getResult[0]
+    return getResult[0]
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message || "Erro ao criar cliente.")
+    } else {
+      throw new Error("Erro desconhecido ao criar cliente.")
+    }
+  }
 }
 
 export const getAllCustomers = async () => {
