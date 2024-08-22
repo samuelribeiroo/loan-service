fa38b3bbc8889f3085c16d70936d3db80cf4ba53
samuelribeiroo <samuelribeirodev@gmail.com>
2024-08-21 23:16:05 -0300

    test: retrieve user by id passed

--- src/test/customerService.test.ts
+++ src/test/customerService.test.ts
@@ -1,9 +1,50 @@
-import { expect } from "@jest/globals"
+import { expect, jest } from "@jest/globals"
+import express from "express"
+import request from "supertest"
+import { getCustomerByID } from "../services/customerService"
+import { CustomerInfo } from "../types/index"
 
-function getSum(a: number, b: number) {
-  return a + b
-}
+jest.setTimeout(8000)
 
-it("", () => {
-  expect(getSum(2, 2)).toBe(4)
+const app = express()
+
+app.use(express.json())
+
+jest.mock("../services/customerService", () => ({
+  getCustomerByID: jest.fn(),
+}))
+
+app.get("/customer/:id", async (request, response) => {
+  try {
+    const { id } = request.params
+    const customer = await getCustomerByID({ id })
+
+    response.json({ customer })
+  } catch (error) {
+    response.sendStatus(404).json({ error: "erro" })
+  }
+})
+
+describe("getUserById", () => {
+  it("Should return a JSON with user", async () => {
+    const mockFakeCustomer: CustomerInfo = {
+      id: "ec0d1287-a1dd-4691-8848-10112f11d049",
+      name: "Geraldo Lucca Nelson Duarte",
+      age: 55,
+      income: 1980.0,
+      location: "MS",
+      cpf: "363.757.821-20",
+    }; 
+
+    // Disclaimer about used data like name, cpf and location
+    // All sensinble data used in this project are provideb by the site 'https://www.4devs.com.br/gerador_de_cpf'
+    // And the main purpose of the project are learning, notthing more.
+
+    (getCustomerByID as jest.MockedFunction<typeof getCustomerByID>).mockResolvedValue(mockFakeCustomer)
+
+    const response = await request(app).get("/customer/ec0d1287-a1dd-4691-8848-10112f11d049").expect(200)
+
+    expect(response.body).toEqual({ customer: mockFakeCustomer })
+    expect(getCustomerByID).toHaveBeenCalledWith({ id: "ec0d1287-a1dd-4691-8848-10112f11d049" })
+  })
 })
