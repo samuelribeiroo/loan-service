7af04ba444ab11fe3434ffd1b603800983b5ebaa
samuelribeiroo <samuelribeirodev@gmail.com>
2024-09-02 19:52:49 -0300

    feat: global error handling added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv"
 import express from "express"
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express"
+import 'express-async-errors'
 import route from "./router/index.js"
 
 dotenv.config()
@@ -11,5 +13,15 @@ const app = express()
 app.use(express.json())
 
 app.use(route)
+
+app.use((error: ErrorRequestHandler, request: Request, response: Response, next: NextFunction) => {
+  console.log(error) 
+  
+  // Consider update console.log to a real service log monitor like winston by example...
+  // this handler up now isnt working bc express by default dont know how to handle with async methods and call error handle
+  // to finish its necessary install a sample lib called: express-async-error e make the import at the main file
+  // is a normal import without save any variable only import 
+  return response.sendStatus(500)
+})
 
 app.listen(PORT, () => console.log(`Server created at: http://localhost:${PORT}`))
